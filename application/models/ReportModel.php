34d929f8d6d292cf6841974fcbd971136e6b4098
sudhirjo <sudhirsalunke199@gmail.com>
2023-06-30 01:15:02 +0530

    Add files via upload

--- application/models/ReportModel.php
+++ application/models/ReportModel.php
@@ -3,9 +3,9 @@ class ReportModel extends CI_Model {
 	
     public function getStockReport($fromDate, $toDate, $productId) {
 	
-		$this->db->select('p.name AS product_name,COALESCE(SUM(pb.total), 0) AS opening,COALESCE(SUM(pb.total), 0) AS total_purchase,COALESCE(SUM(pb.quantity), 0) AS qty,COALESCE(SUM(sb.total), 0) AS total_sale,              (COALESCE(SUM(pb.quantity), 0) - COALESCE(SUM(sb.quantity), 0)) AS stock');
+		$this->db->select('p.name AS product_name,COALESCE(SUM(pb.total), 0) AS opening,COALESCE(SUM(pb.total), 0) AS total_purchase,(sb.rate * sb.quantity + sb.gst ) AS total_sale,(COALESCE(SUM(pb.quantity), 0) - COALESCE(SUM(sb.quantity), 0)) AS stock,pb.quantity');
 			$this->db->from('products as p');
-			$this->db->join('purchase_bills as pb', 'p.id = pb.product_id','left');
+			$this->db->join('purchase_bills as pb', 'p.id = pb.product_id');
 			$this->db->join('sale_bills as sb', 'p.id = sb.product_id','left');
 			if(!empty($fromDate)){				
 			$this->db->where('pb.date >=', $fromDate);
