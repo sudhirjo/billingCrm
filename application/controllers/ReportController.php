ee728b6c02f3ac629921534067dd8e095fe82055
sudhirjo <sudhirsalunke199@gmail.com>
2023-06-30 00:27:46 +0530

    Add files via upload

--- application/controllers/ReportController.php
+++ application/controllers/ReportController.php
@@ -8,23 +8,25 @@ class ReportController extends CI_Controller {
         $this->load->model(array('ReportModel','ProductModel'));
     }
     public function view_stock_report() {
+		
 		$data['products'] = $this->ProductModel->getProductList();
         $data['stockReport'] = $this->ReportModel->getStockReport(
             $this->input->post('from_date'),
             $this->input->post('to_date'),
-            $this->input->post('product_id')
+            $this->input->post('product_id'),
         );
-		
+		    //print_r($this->db->last_query());    die;
 		$this->load->view('common/sidebar');
         $this->load->view('stock_report/view', $data);
     }
 	   public function view_transaction_report() {
         $data['stockReport'] = $this->ReportModel->getTransactionReport(
-            $this->input->get('from_date'),
-            $this->input->get('to_date'),
-            $this->input->get('product_id'),
-			$this->input->get('type'),
+            $this->input->post('from_date'),
+            $this->input->post('to_date'),
+            $this->input->post('product_id'),
+			$this->input->post('type'),
         );
+		//print_r($this->db->last_query());    die;
 		$this->load->view('common/sidebar');
         $this->load->view('transaction _report/view', $data);
     }
